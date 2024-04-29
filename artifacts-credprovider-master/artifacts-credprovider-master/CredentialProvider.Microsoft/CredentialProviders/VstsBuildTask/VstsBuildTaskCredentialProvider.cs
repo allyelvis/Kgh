804d0165953970808c18130b4d2df30253d478a4
Administrator <Administrator@KGHSVR2>
2024-04-29 13:12:47 +0200

    cpl

--- artifacts-credprovider-master/artifacts-credprovider-master/CredentialProvider.Microsoft/CredentialProviders/VstsBuildTask/VstsBuildTaskCredentialProvider.cs
+++ artifacts-credprovider-master/artifacts-credprovider-master/CredentialProvider.Microsoft/CredentialProviders/VstsBuildTask/VstsBuildTaskCredentialProvider.cs
@@ -39,27 +39,6 @@ using NuGetCredentialProvider.Collections.Generic;
 After:
 using System;
 */
-using
-/* Unmerged change from project 'CredentialProvider.Microsoft (net461)'
-Before:
-using System.Collections.Generic;
-
-/* Unmerged change from project 'CredentialProvider.Microsoft (net461)'
-After:
-using System.Collections.Generic;
-/* Unmerged change from project 'CredentialProvider.Microsoft (net461)'
-*/
-
-/* Unmerged change from project 'CredentialProvider.Microsoft (netcoreapp3.1)'
-Before:
-using System.Collections.Generic;
-
-/* Unmerged change from project 'CredentialProvider.Microsoft (net461)'
-After:
-using System.Collections.Generic;
-/* Unmerged change from project 'CredentialProvider.Microsoft (net461)'
-*/
-System.Collections.Generic;
 
 /* Unmerged change from project 'CredentialProvider.Microsoft (net461)'
 Before:
@@ -86,83 +65,83 @@ using ILogger = NuGetCredentialProvider.Logging.ILogger;
 
 namespace NuGetCredentialProvider.CredentialProviders.VstsBuildTask
 {
-	public sealed class VstsBuildTaskCredentialProvider : CredentialProviderBase
-	{
-		private const string Username = "VssSessionToken";
+    public sealed class VstsBuildTaskCredentialProvider : CredentialProviderBase
+    {
+        private const string Username = "VssSessionToken";
 
-		public VstsBuildTaskCredentialProvider(ILogger logger)
-			: base(logger)
-		{
-		}
+        public VstsBuildTaskCredentialProvider(ILogger logger)
+            : base(logger)
+        {
+        }
 
-		protected override string LoggingName => nameof(VstsBuildTaskCredentialProvider);
+        protected override string LoggingName => nameof(VstsBuildTaskCredentialProvider);
 
-		public override bool IsCachable { get { return false; } }
+        public override bool IsCachable { get { return false; } }
 
-		public override Task<bool> CanProvideCredentialsAsync(Uri uri)
-		{
-			string uriPrefixesString = Environment.GetEnvironmentVariable(EnvUtil.BuildTaskUriPrefixes);
-			string accessToken = Environment.GetEnvironmentVariable(EnvUtil.BuildTaskAccessToken);
+        public override Task<bool> CanProvideCredentialsAsync(Uri uri)
+        {
+            string uriPrefixesString = Environment.GetEnvironmentVariable(EnvUtil.BuildTaskUriPrefixes);
+            string accessToken = Environment.GetEnvironmentVariable(EnvUtil.BuildTaskAccessToken);
 
-			bool useBuildTaskCredProvider = string.IsNullOrWhiteSpace(uriPrefixesString) == false && string.IsNullOrWhiteSpace(accessToken) == false;
-			if (useBuildTaskCredProvider == true)
-			{
-				return Task.FromResult(true);
-			}
+            bool useBuildTaskCredProvider = string.IsNullOrWhiteSpace(uriPrefixesString) == false && string.IsNullOrWhiteSpace(accessToken) == false;
+            if (useBuildTaskCredProvider == true)
+            {
+                return Task.FromResult(true);
+            }
 
-			Verbose(Resources.BuildTaskEnvVarError);
-			return Task.FromResult(false);
-		}
+            Verbose(Resources.BuildTaskEnvVarError);
+            return Task.FromResult(false);
+        }
 
-		public override Task<GetAuthenticationCredentialsResponse> HandleRequestAsync(GetAuthenticationCredentialsRequest request, CancellationToken cancellationToken)
-		{
-			cancellationToken.ThrowIfCancellationRequested();
+        public override Task<GetAuthenticationCredentialsResponse> HandleRequestAsync(GetAuthenticationCredentialsRequest request, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
 
-			string uriPrefixesString = Environment.GetEnvironmentVariable(EnvUtil.BuildTaskUriPrefixes);
-			string accessToken = Environment.GetEnvironmentVariable(EnvUtil.BuildTaskAccessToken);
+            string uriPrefixesString = Environment.GetEnvironmentVariable(EnvUtil.BuildTaskUriPrefixes);
+            string accessToken = Environment.GetEnvironmentVariable(EnvUtil.BuildTaskAccessToken);
 
-			Verbose(string.Format(Resources.IsRetry, request.IsRetry));
+            Verbose(string.Format(Resources.IsRetry, request.IsRetry));
 
-			string[] uriPrefixes = uriPrefixesString.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-			Verbose(Resources.BuildTaskUriPrefixes);
-			foreach (var uriPrefix in uriPrefixes)
-			{
-				Verbose($"{uriPrefix}");
-			}
+            string[] uriPrefixes = uriPrefixesString.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            Verbose(Resources.BuildTaskUriPrefixes);
+            foreach (var uriPrefix in uriPrefixes)
+            {
+                Verbose($"{uriPrefix}");
+            }
 
-			string uriString = request.Uri.AbsoluteUri;
-			string matchedPrefix = uriPrefixes.FirstOrDefault(prefix => uriString.StartsWith(prefix, StringComparison.OrdinalIgnoreCase));
-			Verbose(string.Format(Resources.BuildTaskMatchedPrefix, matchedPrefix != null ? matchedPrefix : Resources.BuildTaskNoMatchingPrefixes));
+            string uriString = request.Uri.AbsoluteUri;
+            string matchedPrefix = uriPrefixes.FirstOrDefault(prefix => uriString.StartsWith(prefix, StringComparison.OrdinalIgnoreCase));
+            Verbose(string.Format(Resources.BuildTaskMatchedPrefix, matchedPrefix != null ? matchedPrefix : Resources.BuildTaskNoMatchingPrefixes));
 
-			if (matchedPrefix == null)
-			{
-				Verbose(Resources.BuildTaskNoPrefixMatch);
-				return this.GetResponse(
-					null,
-					null,
-					Resources.BuildTaskNoPrefixMatch,
-					MessageResponseCode.Error);
-			}
+            if (matchedPrefix == null)
+            {
+                Verbose(Resources.BuildTaskNoPrefixMatch);
+                return this.GetResponse(
+                    null,
+                    null,
+                    Resources.BuildTaskNoPrefixMatch,
+                    MessageResponseCode.Error);
+            }
 
-			Verbose(string.Format(Resources.BuildTaskEndpointMatchingUrlFound, uriString));
-			return this.GetResponse(
-					Username,
-					accessToken,
-					null,
-					MessageResponseCode.Success);
-		}
+            Verbose(string.Format(Resources.BuildTaskEndpointMatchingUrlFound, uriString));
+            return this.GetResponse(
+                    Username,
+                    accessToken,
+                    null,
+                    MessageResponseCode.Success);
+        }
 
-		private Task<GetAuthenticationCredentialsResponse> GetResponse(string username, string password, string message, MessageResponseCode responseCode)
-		{
-			return Task.FromResult(new GetAuthenticationCredentialsResponse(
-					username: username,
-					password: password,
-					message: message,
-					authenticationTypes: new List<string>
-					{
-						"Basic"
-					},
-					responseCode: responseCode));
-		}
-	}
+        private Task<GetAuthenticationCredentialsResponse> GetResponse(string username, string password, string message, MessageResponseCode responseCode)
+        {
+            return Task.FromResult(new GetAuthenticationCredentialsResponse(
+                    username: username,
+                    password: password,
+                    message: message,
+                    authenticationTypes: new List<string>
+                    {
+                        "Basic"
+                    },
+                    responseCode: responseCode));
+        }
+    }
 }
